6f9582a850a9b7247caaf3e22785ea22641139f3
Armatriwah <96682868+Gedonovic@users.noreply.github.com>
2022-10-06 11:35:02 +0700

    Update Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,7 +4,7 @@
             <div class="col-lg-12 col-md-12 col-12">
                 <p>
                     Design 
-                    A<a href="asd">B</a>
+                    Aaaaaaaaaaaaaaaaaaaaa<a href="asd">B</a>
                 </p>
             </div>
         </div>
